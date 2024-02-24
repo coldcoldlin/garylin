f00ae7acbb62f41020e5edfa7eacfe2cd379601d
coldcoldlin <132245007+coldcoldlin@users.noreply.github.com>
2024-02-24 10:22:06 +0800

    todolist

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="add">
+      <input type="text" id="" class="add-text">
+      <button class="add-Todo" type="button">新增</button>
+    </div>
+  </main>
+</template>
